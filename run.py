076ad97dcbd06e28cd336452507b003980e1c3e7
Xitong Wei <116434477+mobooV@users.noreply.github.com>
2024-10-29 23:13:11 +0800

    Update run.py（加了一段bgm播放的
    开始游戏自动循环播放bgm，播哪个自己选

--- run.py
+++ run.py
@@ -6,6 +6,11 @@ from mainform import MainWindow
 import sys
 
 if __name__ == "__main__":
+    # 整点bgm
+    pygame.mixer.init()  # 初始化pygame音频模块
+    pygame.mixer.music.load("gamemusic.mp3")  # 替换为音乐文件路径（两个都很适合嗯
+    pygame.mixer.music.play(-1)  # -1表示循环播放
+    
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     app = QtWidgets.QApplication([])
     #前面的废话没啥用
