b1ddd5fd088ed0ddc3159e2bbf76f390bc9a65e8
Xitong Wei <116434477+mobooV@users.noreply.github.com>
2024-10-30 12:22:33 +0800

    Update run.py
    改为：先启动开始界面，输入id和密码完毕点击确认按钮后，再启动主游戏界面，具体改动看注释owo

--- run.py
+++ run.py
@@ -5,16 +5,32 @@ from PyQt5 import QtCore, QtWidgets
 from mainform import MainWindow
 import sys
 import pygame
+from startwindow import MyMainWindow
+
+
 
 if __name__ == "__main__":
     # 整点bgm
     pygame.mixer.init()  # 初始化pygame音频模块
     pygame.mixer.music.load("gamemusic.mp3")  # 替换为音乐文件路径（两个都很适合嗯
     pygame.mixer.music.play(-1)  # -1表示循环播放
-    
-    QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
-    app = QtWidgets.QApplication([])
-    #前面的废话没啥用
-    window = MainWindow()
-    #后面的废话没啥用
-    sys.exit(app.exec())
+
+    # 这里改成直接启动getID里的开始界面
+    # 设置为: 点击开始界面的“确认”后，才进入到主游戏界面
+    app = QtWidgets.QApplication(sys.argv)
+    mainwindow = MyMainWindow()
+    mainwindow.show()
+    sys.exit(app.exec_())
+
+    # QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
+    # app2 = QtWidgets.QApplication([])
+    # app2 = QtWidgets.QApplication(sys.argv)
+    # 前面的废话没啥用
+    # window = MainWindow()
+    # window.show()
+    # 后面的废话没啥用
+    # sys.exit(app2.exec())
+
+
+
+
