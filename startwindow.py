abc793086e96d321853f9bc53a64024518fb82df
Xitong Wei <116434477+mobooV@users.noreply.github.com>
2024-10-31 14:27:50 +0800

    美化了一下

--- startwindow.py
+++ startwindow.py
@@ -20,16 +20,44 @@ class MyMainWindow(QtWidgets.QMainWindow):
 
         # 连接信号与槽
         self.ui.pushButton.clicked.connect(self.on_button_clicked)
+        self.ui.registerButton.clicked.connect(self.register_button_clicked)
 
+    # 这里是点击注册按钮后执行的操作
+    def register_button_clicked(self):
+        # 点击后不会直接跳转到游戏界面
+        register_userID = self.ui.lineEdit.text()
+        register_password = self.ui.lineEdit_2.text()
+        print("注册ID为：", register_userID, "\n注册密码为：", register_password)
+
+        ###### 这里写数据库相关代码 ######
+        # 功能：把register_userID和对应的register_password写入数据库
+
+
+
+
+
+        ################
+
+        print("注册成功!")  # 测试代码
+
+
+    # 这里是点击登录按钮后执行的操作
     def on_button_clicked(self):
-        # 这里是点击确认按钮后执行的操作
-        # 点击确认后可以直接跳转到游戏界面
-        print("button clicked!")  # 测试代码
+        print("登录成功!")  # 测试代码
 
         # 获取用户输入
         userID = self.ui.lineEdit.text()
         password = self.ui.lineEdit_2.text()
         print("ID: ", userID, "\npassword: ", password, "\n")  # 测试代码
+
+        ######### 这里是数据库相关代码 ########
+        # 功能：从数据库里读取和userID匹配的通关数据
+        # 有没有可能实现“检测密码是否正确”的功能？=_=
+
+
+
+
+        ############
 
         # 启动主游戏界面
         self.gameMainWindow = MainWindow()
